5e930e28dcfafc62030cc7d700d6cf914caf5a22
EduarDG09 <eduardgm201@gmail.com>
2021-04-26 18:34:51 -0400

    Mejorados algunos scripts y funciones, añadida el algoritmo para guardar y mostrar links recientes

--- script.js
+++ script.js
@@ -2,14 +2,62 @@ document.addEventListener("DOMContentLoaded", function () {
   const linkInput = document.getElementById("short-input");
   const linksContainer = document.getElementById("links-container");
   const linkBtn = document.getElementById("short-btn");
+  const shortenLink = mainService();
 
+  loadRecentLinks();
   linkBtn.addEventListener("click", shortenLink);
 
-  async function shortenLink() {
-    const link = inputValidation(linkInput);
-    if (link !== false) {
-      linkInput.value = "";
-      saveLink(updateBox(await getShortenedLink(drawBox(link))));
+  function loadRecentLinks() {
+    const savedLinks = getLinksFromStorage();
+    if (savedLinks !== null) {
+      const linksContainerTitle = document.createElement("h3");
+      linksContainerTitle.classList.add("links-container__title");
+      linksContainerTitle.textContent = "your recent links";
+      linksContainer.parentNode.insertBefore(
+        linksContainerTitle,
+        linksContainer
+      );
+      savedLinks.forEach((savedLink) => {
+        updateBox(drawBox({ new: false, links: savedLink }));
+      });
+    }
+  }
+
+  function mainService() {
+    let processing = 0;
+    let queue = [];
+    return async (link) => {
+      if (processing < 5) {
+        const link = inputValidation(linkInput);
+        if (link !== false) {
+          if (queue.includes(link))
+            return console.log("El link introducido ya está siendo procesado.");
+          else queue.push(link);
+          processing++;
+          linkInput.value = "";
+          saveLink(
+            updateBox(await getShortenedLink(drawBox(verifyExistingLink(link))))
+          );
+          if (queue.indexOf(link) !== -1) queue.splice(queue.indexOf(link), 1);
+          processing--;
+          return console.log("Link procesado correctamente!");
+        }
+      } else {
+        console.log("No se pueden procesar mas de 5 links a la vez!");
+        return;
+      }
+    };
+  }
+
+  function verifyExistingLink(link) {
+    try {
+      const match = getLinksFromStorage().find((savedLink) =>
+        Object.values(savedLink).includes(link)
+      );
+      if (match !== undefined) return { new: false, links: match };
+      else throw Error;
+    } catch {
+      return { new: true, link: link };
     }
   }
 
@@ -17,53 +65,68 @@ document.addEventListener("DOMContentLoaded", function () {
     return JSON.parse(localStorage.getItem("links"));
   }
 
-  function saveLink(links) {
-    const shortenedLink = { ...links };
-    const savedLinks = getLinksFromStorage();
-    if (savedLinks === null) {
-      localStorage.setItem("links", JSON.stringify(new Array(shortenedLink)));
-    } else {
-      savedLinks.push(shortenedLink);
-      localStorage.setItem("links", JSON.stringify(savedLinks));
+  function saveLink(info) {
+    if (info.new === false || info.error === true) return;
+    else {
+      const shortenedLink = { ...info.links };
+      const savedLinks = getLinksFromStorage();
+      if (savedLinks === null) {
+        localStorage.setItem("links", JSON.stringify(new Array(shortenedLink)));
+      } else {
+        savedLinks.push(shortenedLink);
+        if (savedLinks.length > 5) savedLinks.shift();
+        localStorage.setItem("links", JSON.stringify(savedLinks));
+      }
     }
   }
 
-  function updateBox(res) {
-    if (res.error !== true) {
-      const shortLinkField = res.box.querySelector(
-        ".links-card__shortened-link"
-      );
-      const cardBtn = res.box.querySelector(".btn");
-      shortLinkField.textContent = res.links.short;
-      shortLinkField.href = res.links.short;
-      shortLinkField.target = "_blank";
-      cardBtn.textContent = "Copy!";
-      cardBtn.addEventListener("click", async function () {
-        try {
-          await navigator.clipboard.writeText(res.links.short);
-          console.log("Link copied to clipboard");
-        } catch (err) {
-          console.log(err);
-        }
-      });
-      return { ...res.links };
+  function updateBox(info) {
+    const shortLinkField = info.box.querySelector(
+      ".links-card__shortened-link"
+    );
+    const cardBtn = info.box.querySelector(".btn");
+    if (info.error) {
+      shortLinkField.textContent = "Error while processing the link.";
+      shortLinkField.classList.add("error");
+      cardBtn.textContent = "Error";
+      cardBtn.classList.add("error");
+      return info;
     }
+    shortLinkField.textContent = info.links.short;
+    shortLinkField.href = info.links.short;
+    shortLinkField.target = "_blank";
+    cardBtn.textContent = "Copy";
+    cardBtn.addEventListener("click", async function () {
+      try {
+        await navigator.clipboard.writeText(info.links.short);
+        this.classList.add("btn--dark");
+        this.textContent = "Copied!";
+        setTimeout(() => {
+          this.classList.remove("btn--dark"), (this.textContent = "Copy");
+        }, 3000);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+    return info;
   }
 
-  async function getShortenedLink(data) {
-    try {
-      const request = await fetch(
-        `https://api.shrtco.de/v2/shorten?url=${data.link}`
-      );
-      const response = await request.json();
-      const links = {
-        short: response.result.full_short_link,
-        original: response.result.original_link,
-      };
-      return { error: false, box: data.box, links: { ...links } };
-    } catch (err) {
-      console.log(err);
-      return true, data.box;
+  async function getShortenedLink(info) {
+    if (info.new === false) return info;
+    else {
+      try {
+        const request = await fetch(
+          `https://api.shrtco.de/v2/shorten?url=${info.link}`
+        );
+        const response = await request.json();
+        const links = {
+          original: response.result.original_link,
+          short: response.result.full_short_link,
+        };
+        return { new: info.new, links: links, box: info.box };
+      } catch (err) {
+        return { error: true, box: info.box };
+      }
     }
   }
 
@@ -75,18 +138,30 @@ document.addEventListener("DOMContentLoaded", function () {
     return value;
   }
 
-  function drawBox(link) {
+  function drawBox(info) {
+    if (linksContainer.childElementCount > 4)
+      undrawBox(linksContainer.lastElementChild);
     const box = document.createElement("div");
-    let displayLink = link;
+    let userLink = (displayLink = info.new ? info.link : info.links.original);
     box.classList.add("links-card", "resize-height");
-    if (link.length > 50) displayLink = link.slice(0, 51) + "...";
+    if (userLink.length > 50) displayLink = userLink.slice(0, 51) + "...";
     box.innerHTML = `
-    <h2 class="links-card__link">${displayLink}</h2>
-    <a class="links-card__shortened-link"></a>
-    <a id="links-btn" class="btn links-btn"><span class="lnr lnr-sync spin"></span></a>
+      <h2 class="links-card__link">${displayLink}</h2>
+      <a class="links-card__shortened-link"></a>
+      <a id="links-btn" class="btn links-btn"><span class="lnr lnr-sync spin"></span></a>
     `;
     linksContainer.prepend(box);
-    return { link: link, box: box };
+    box.addEventListener("animationend", function () {
+      this.classList.remove("resize-height");
+    });
+    return { box: box, ...info };
+  }
+
+  function undrawBox(box) {
+    box.style.animation = "resize-height 350ms ease-out reverse forwards";
+    box.addEventListener("animationend", function () {
+      this.remove();
+    });
   }
 
   function isValidURL(url) {
